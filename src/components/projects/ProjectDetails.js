490b36f38f5f28256254de0eb6ae92465203368f
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-11-10 08:33:30 +0100

    get project details from the API (ProjectDetails)

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Link, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class ProjectDetails extends Component {
   state = {}
 
+  componentDidMount(){
+      this.getSingleProject();
+  }
+
+  getSingleProject = () => {
+      const { params } = this.props.match;
+      axios.get(`http://localhost:5000/api/projects/${params.id}`)
+      .then( responseFromApi =>{
+          const theProject = responseFromApi.data;
+          this.setState(theProject);
+      })
+      .catch((err)=>{
+          console.log(err)
+      })
+  }
+
   render(){
-    return <div>Welcome to project details page!</div>
+    return(
+      <div>
+        <h1>{this.state.title}</h1>
+        <p>{this.state.description}</p>
+        <Link to={'/projects'}>Back to projects</Link>
+      </div>
+    )
   }
 }
 
