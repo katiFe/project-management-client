306ff5bc93119e949b092be77c59a13b569d1f18
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-11-10 08:30:28 +0100

    add routing, Navbar and ProjectDetails

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link, Redirect } from 'react-router-dom';
+
+class ProjectDetails extends Component {
+  state = {}
+
+  render(){
+    return <div>Welcome to project details page!</div>
+  }
+}
+
+export default ProjectDetails;
