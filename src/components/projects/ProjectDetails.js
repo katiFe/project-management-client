d5a2fda5693a8f172c047e86d43fd56cb49f2c4f
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-11-10 08:43:36 +0100

    functionality to update projects (EditProject)

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,35 +1,43 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import EditProject from './EditProject';
 
 class ProjectDetails extends Component {
-  state = {}
+    state = {}
 
-  componentDidMount(){
-      this.getSingleProject();
-  }
+    componentDidMount() {
+        this.getSingleProject();
+    }
 
-  getSingleProject = () => {
-      const { params } = this.props.match;
-      axios.get(`http://localhost:5000/api/projects/${params.id}`)
-      .then( responseFromApi =>{
-          const theProject = responseFromApi.data;
-          this.setState(theProject);
-      })
-      .catch((err)=>{
-          console.log(err)
-      })
-  }
+    getSingleProject = () => {
+        const { params } = this.props.match;
+        axios.get(`http://localhost:5000/api/projects/${params.id}`)
+            .then(responseFromApi => {
+                const theProject = responseFromApi.data;
+                this.setState(theProject);
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
 
-  render(){
-    return(
-      <div>
-        <h1>{this.state.title}</h1>
-        <p>{this.state.description}</p>
-        <Link to={'/projects'}>Back to projects</Link>
-      </div>
-    )
-  }
+    renderEditForm = () => {
+        if (this.state.title) {
+            return <EditProject theProject={this.state} getTheProject={this.getSingleProject} />
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.title}</h1>
+                <p>{this.state.description}</p>
+                <div>{this.renderEditForm()} </div>
+                <Link to={'/projects'}>Back to projects</Link>
+            </div>
+        )
+    }
 }
 
 export default ProjectDetails;
